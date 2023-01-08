49caee0cb2ced997be149781ef36f80ad5132531
nightyknite <nightyknite3@gmail.com>
2023-01-08 20:53:46 +0900

    ts2339対応

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
 defineProps(['start', 'limit', 'keyword']);
+const emit = defineEmits(['update', 'btnClick', 'update:start', 'update:limit', 'update:keyword']);
+const handleInputStart = (event: Event) => {
+  emit('update:start', (event.target as HTMLInputElement).value);
+};
+const handleInputLimit = (event: Event) => {
+  emit('update:limit', (event.target as HTMLInputElement).value);
+};
+const handleInputKeyword = (event: Event) => {
+  emit('update:keyword', (event.target as HTMLInputElement).value);
+};
+const btnClick = () => {
+  emit('btnClick');
+};
 </script>
 <template>
   <div>
@@ -8,10 +21,10 @@ defineProps(['start', 'limit', 'keyword']);
         <a class="navbar-brand" href="#">Event Calendar</a>
         <div class="collapse navbar-collapse" >
           <form class="d-flex">
-            <input id="start" :value="start" @input="$emit('update:start', $event.target.value)" class="form-control me-sm-2" type="text" placeholder="00:00" style="width:5em" maxlength="5" >
-            <input id="limit" :value="limit"  @input="$emit('update:limit', $event.target.value)" class="form-control me-sm-2" type="text" placeholder="limit" style="width:4em" maxlength="4" >
-            <input id="keyword" :value="keyword" @input="$emit('update:keyword', $event.target.value)" class="form-control me-sm-2" type="search" placeholder="keyword" >
-            <button id="search" class="btn btn-outline-success my-2 my-sm-0" type="button" @click="$emit('btnClick')">Search</button>    
+            <input id="start" :value="start" @input="handleInputStart($event)" class="form-control me-sm-2" type="text" placeholder="00:00" style="width:5em" maxlength="5" >
+            <input id="limit" :value="limit"  @input="handleInputLimit($event)" class="form-control me-sm-2" type="text" placeholder="limit" style="width:4em" maxlength="4" >
+            <input id="keyword" :value="keyword" @input="handleInputKeyword($event)" class="form-control me-sm-2" type="search" placeholder="keyword" >
+            <button id="search" class="btn btn-outline-success my-2 my-sm-0" type="button" @click="btnClick()">Search</button>    
           </form>
         </div>
       </div>
